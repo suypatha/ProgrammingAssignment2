4486c0b887109b0d95cdb72e759b148b94d7bc90
suypatha <suyogp@gmail.com>
2015-07-25 16:39:02 -0400

    updated assignment function code

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Put comments here that give an overall description of what your
+## functions do
 
+## Write a short comment describing this function
+
+makeCacheMatrix <-function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    #get function obtains the stored value
+    get <- function() x
+    #sets the function to be used 
+    setsolve <- function(solve) m <<- solve
+    #gets the name of fucntion to be used
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <-function(x, ...) {
+    ## assign the return value of getsolve function (from makeCacheMatri) to m
+    m <- x$getsolve()
+    ## if m is notnull use cached data
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
