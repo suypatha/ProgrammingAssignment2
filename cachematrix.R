5583cb506becb1c84f0f9d52fb406cf5539776d0
suypatha <suyogp@gmail.com>
2015-07-25 19:01:12 -0400

    additinal comments to the function code

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,18 @@
-### Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching
+## the inverse of a matrix rather than computing it repeatedly (there are also alternatives
+## to matrix inversion that we will not discuss here). Your assignment is to write a pair 
+##of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its 
+## inverse.
 
 
 makeCacheMatrix <-function(x = matrix()) {
-    #Initialize inverse metric (for first run)
+    #Initialize inverse mztrix (for first run)
     if (!(exists("m"))){
         m<<-NULL
     }
-    #Initialize old matrix vlaue (for firat run) to comapare latter
+    #Initialize old matrix vlaue (for first run) to comapare latter
     if (!exists("oldmatrix")) {
         oldmatrix<<-x
     }
@@ -42,7 +39,10 @@ makeCacheMatrix <-function(x = matrix()) {
          setsolve = setsolve,
          getsolve = getsolve,getoldmatrix=getoldmatrix,getcurrmatrix=getcurrmatrix)
 }
-## Write a short comment describing this function
+
+## cacheSolve: This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above. If the inverse has already been calculated (and the matrix has
+## not changed), then cacheSolve should retrieve the inverse from the cache
 
 cacheSolve <-function(x, ...) {
     ##Verify if new input matrix is same as last run
