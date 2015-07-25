b2b6c55f50b11e9aff9f6954e199b7b963e74217
suypatha <suyogp@gmail.com>
2015-07-25 18:51:30 -0400

    completed assignment function code

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+### Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
@@ -7,37 +7,61 @@
 ## functions do
 
 ## Write a short comment describing this function
+
+
 
 makeCacheMatrix <-function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+    #Initialize inverse metric (for first run)
+    if (!(exists("m"))){
+        m<<-NULL
     }
+    #Initialize old matrix vlaue (for firat run) to comapare latter
+    if (!exists("oldmatrix")) {
+        oldmatrix<<-x
+    }
+    # set current matrix value
+    currmatrix<<-x
+    
+    #Function to get old metrix value
+    getoldmatrix <- function() oldmatrix
+    
+    #function to get current matrix value
+    getcurrmatrix<- function() currmatrix
+    
     #get function obtains the stored value
     get <- function() x
-    #sets the function to be used 
+    
+    #sets the inverse of input matrix value
     setsolve <- function(solve) m <<- solve
-    #gets the name of fucntion to be used
+    
+    #gets the inverse of input matrix value
     getsolve <- function() m
-    list(set = set, get = get,
+    
+    #output list of all required functions. This will be input to cacheSolve 
+    list(get = get,
          setsolve = setsolve,
-         getsolve = getsolve)
+         getsolve = getsolve,getoldmatrix=getoldmatrix,getcurrmatrix=getcurrmatrix)
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <-function(x, ...) {
-    ## assign the return value of getsolve function (from makeCacheMatri) to m
-    m <- x$getsolve()
-    ## if m is notnull use cached data
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
+    ##Verify if new input matrix is same as last run
+    if (identical(x$getoldmatrix(), x$getcurrmatrix())) {
+        ## Check if inverse was calculated
+        if (!is.null(m)){
+            ## If yes then use cached value
+            message("getting cached data")
+            return(m)
+        }
     }
+    # If this is a first run then get the matrix 
     data <- x$get()
+    #compute inverse
     m <- solve(data, ...)
+    #set the value for makecacheMatrix
     x$setsolve(m)
+    #Set the value of current run as oldmatrix to comapre for next run
+    oldmatrix<<-data
+    #output inverse
     m
 }
